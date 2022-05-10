5d34462b3e3c5cd567a1e3b8d3d0373757e9281c
Francois Roland <froland@users.noreply.github.com>
2022-05-10 16:47:11 +0000

    Generate project structure.

---
+++ src/main/java/be/eafcuccle/projint/flywayh2demo/FlywayH2DemoApplication.java
@@ -0,0 +1,13 @@
+package be.eafcuccle.projint.flywayh2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayH2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayH2DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/eafcuccle/projint/flywayh2demo/FlywayH2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package be.eafcuccle.projint.flywayh2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayH2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
